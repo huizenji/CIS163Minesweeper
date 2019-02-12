7bc0b6406c2d4311ae7f4182331d51dda958d227
huizenji <47357244+huizenji@users.noreply.github.com>
2019-02-12 17:47:33 -0500

    Update MineSweeperGame.java

--- MineSweeperGame.java
+++ MineSweeperGame.java
@@ -1,4 +1,4 @@
-package W19Project2GIVETOSTUDENTS;
+package project2;
 
 
 import java.util.Random;
@@ -6,19 +6,20 @@ import java.util.Random;
 public class MineSweeperGame {
 	private Cell[][] board;
 	private GameStatus status;
+	private int totalMineCount = 10;
 
 	public MineSweeperGame() {
 		status = GameStatus.NotOverYet;
-		board = new Cell[5][5];
+		board = new Cell[10][10];
 		setEmpty();
 		layMines (7);
 		
 	}
 
 	private void setEmpty() {
-		for (int r = 0; r < 5; r++)
-			for (int c = 0; c < 5; c++)
-				board[r][c] = new Cell(false, false);  // totally clear.
+		for (int r = 0; r < 10; r++)
+			for (int c = 0; c < 10; c++)
+				board[r][c] = new Cell(false, false, false);  // totally clear.
 	}
 
 	public Cell getCell(int row, int col) {
@@ -30,8 +31,9 @@ public class MineSweeperGame {
 
 		if (board[row][col].isMine())   // did I lose
 			status = GameStatus.Lost;
-		else {
-			status = GameStatus.Won;    // did I win
+		else { // every cell exposed?
+
+			status = GameStatus.WON;    // did I win
 
 		}
 	}
@@ -51,8 +53,8 @@ public class MineSweeperGame {
 
 		Random random = new Random();
 		while (i < mineCount) {			// perhaps the loop will never end :)
-			int c = random.nextInt(5);
-			int r = random.nextInt(5);
+			int c = random.nextInt(10);
+			int r = random.nextInt(10);
 
 			if (!board[r][c].isMine()) {
 				board[r][c].setMine(true);
@@ -60,11 +62,23 @@ public class MineSweeperGame {
 			}
 		}
 	}
+
+	public int neighborCount(int row, int col){
+		int count = 0;
+		if (!board[row][col].isMine()) {
+			for (int scannedRow = row + 1; scannedRow <= row + 1 ||
+					scannedRow >= row - 1; scannedRow--) {
+				for (int scannedCol = col - 1; scannedCol <= col + 1; scannedCol++) {
+					if (board[scannedRow][scannedCol].isMine())
+						count++;
+				}
+			}
+		}
+		return count;
+	}
 }
 
 
 
 
 	//  a non-recursive from .... it would be much easier to use recursion
-
-
