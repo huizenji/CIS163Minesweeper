923b0b85382d684026550f106fac0ea2da3ecb7c
huizenji <47357244+huizenji@users.noreply.github.com>
2019-02-17 10:37:44 -0500

    Update MineSweeperGUI.java

--- MineSweeperGUI.java
+++ MineSweeperGUI.java
@@ -4,6 +4,8 @@ package W19Project2GIVETOSTUDENTS;
 import javax.swing.*;
 
 public class MineSweeperGUI {
+	public static String boardSizeStr = JOptionPane.showInputDialog (null, "Enter in the " +
+			"size of the board (3-30): ");
 	public static void main (String[] args)
 	{
 		JFrame frame = new JFrame ("Mine Sweeper!");
