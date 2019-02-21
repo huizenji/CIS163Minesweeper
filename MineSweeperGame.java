fd39e3dbf5de501a61bdd8ad407468a8a9b20b62
huizenji <47357244+huizenji@users.noreply.github.com>
2019-02-20 20:59:54 -0500

    Update MineSweeperGame.java
    Everything should be good except recursion stuff?

--- MineSweeperGame.java
+++ MineSweeperGame.java
@@ -6,22 +6,21 @@ import java.util.Random;
 public class MineSweeperGame {
     private Cell[][] board;
     private GameStatus status;
-    private static int totalMineCount;
+    private int totalMineCount;
     private static String totalMineCountStr = "";
-    private static int boardSize;
+    private int boardSize;
     private static String boardSizeStr = "";
 
     public MineSweeperGame() {
         status = GameStatus.NotOverYet;
-        boardSize = 10;
-        totalMineCount = 10;
+        boardSize = Integer.parseInt(boardSizeStr);
+        totalMineCount = Integer.parseInt(totalMineCountStr);
         board = new Cell[boardSize][boardSize];
         setEmpty();
         layMines(totalMineCount);
-
     }
 
-    public static int getBoardSize() {
+    public int getBoardSize() {
         return boardSize;
     }
 
@@ -29,70 +28,76 @@ public class MineSweeperGame {
         do {
             if (boardSizeStr.length() > 0) {
                 JOptionPane.showMessageDialog(null, "Please choose a" +
-                        " number between 3 and 30", "ERROR", JOptionPane.ERROR_MESSAGE);
+                        " number between 3 and 30", "ERROR",
+                        JOptionPane.ERROR_MESSAGE);
             }
             boardSizeStr = JOptionPane.showInputDialog(null, "Enter " +
                     "in the size of the board (3-30): ");
         } while (boardSizeStr.equals("") ||
-                Integer.parseInt(boardSizeStr) < 3 || Integer.parseInt(boardSizeStr) > 30);
-
-        boardSize = Integer.parseInt(boardSizeStr);
+                Integer.parseInt(boardSizeStr) < 3 ||
+                Integer.parseInt(boardSizeStr) > 30);
     }
 
     public static void promptNumberOfMines() {
-        totalMineCountStr = JOptionPane.showInputDialog(null, "Enter in " +
-                "the desired number of mines (default is 10): ");
-        if (Integer.parseInt(totalMineCountStr) >= 1 &&
-                Integer.parseInt(totalMineCountStr) < Integer.parseInt(boardSizeStr))
-            totalMineCount = Integer.parseInt(totalMineCountStr);
+        totalMineCountStr = JOptionPane.showInputDialog(null,
+                "Enter in the desired number of mines " +
+                        "(default is 10): ");
+
+        // set mines to 10 if less than 1 or over cell count
+        if (Integer.parseInt(totalMineCountStr) < 1 ||
+                Integer.parseInt(totalMineCountStr) >
+                        Integer.parseInt(boardSizeStr) *
+                                Integer.parseInt(boardSizeStr)) {
+            totalMineCountStr = "10";
+        }
     }
 
     private void setEmpty() {
         for (int r = 0; r < boardSize; r++)
             for (int c = 0; c < boardSize; c++)
-                board[r][c] = new Cell(false, false, false);  // totally clear.
+                board[r][c] = new Cell(false, false, false);
     }
 
-	public Cell getCell(int row, int col) {
-		return board[row][col];
-	}
+    public Cell getCell(int row, int col) {
+        return board[row][col];
+    }
 
-	public void select(int row, int col) {
-		board[row][col].setExposed(true);
+    public void select(int row, int col) {
+        board[row][col].setExposed(true);
 
-		if (board[row][col].isMine())   // did I lose
-			status = GameStatus.Lost;
-		else { // every cell exposed?
+        if (board[row][col].isMine())   // did I lose
+            status = GameStatus.Lost;
+        else { // every cell exposed?
 
-			status = GameStatus.WON;    // did I win
+            status = GameStatus.WON;    // did I win
 
-		}
-	}
+        }
+    }
 
-	public GameStatus getGameStatus() {
-		return status;
-	}
+    public GameStatus getGameStatus() {
+        return status;
+    }
 
-	public void reset() {
-		status = GameStatus.NotOverYet;
-		setEmpty();
-		layMines (10);
-	}
+    public void reset() {
+        status = GameStatus.NotOverYet;
+        setEmpty();
+        layMines(10);
+    }
 
-	private void layMines(int mineCount) {
-		int i = 0;		// ensure all mines are set in place
+    private void layMines(int mineCount) {
+        int i = 0;        // ensure all mines are set in place
 
-		Random random = new Random();
-		while (i < mineCount) {			// perhaps the loop will never end :)
-			int c = random.nextInt(10);
-			int r = random.nextInt(10);
+        Random random = new Random();
+        while (i < mineCount) {            // perhaps the loop will never end :)
+            int c = random.nextInt(boardSize);
+            int r = random.nextInt(boardSize);
 
-			if (!board[r][c].isMine()) {
-				board[r][c].setMine(true);
-				i++;
-			}
-		}
-	}
+            if (!board[r][c].isMine()) {
+                board[r][c].setMine(true);
+                i++;
+            }
+        }
+    }
 
     /******************************************************************
      * This method counts the number of mines directly surrounding the
@@ -127,10 +132,8 @@ public class MineSweeperGame {
             }
         }
     }
-	
+
 }
 
 
-
-
-	//  a non-recursive from .... it would be much easier to use recursion
+//  a non-recursive from .... it would be much easier to use recursion
