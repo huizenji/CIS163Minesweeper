afbd708727a4d345dca7258424e5a91cddd478ee
huizenji <47357244+huizenji@users.noreply.github.com>
2019-02-20 23:19:55 -0500

    Update MineSweeperGame.java
    Fixed neighborCount

--- MineSweeperGame.java
+++ MineSweeperGame.java
@@ -7,8 +7,8 @@ public class MineSweeperGame {
     private Cell[][] board;
     private GameStatus status;
     private int totalMineCount;
-    private static String totalMineCountStr = "";
     private int boardSize;
+    private static String totalMineCountStr = "";
     private static String boardSizeStr = "";
 
     public MineSweeperGame() {
@@ -67,9 +67,10 @@ public class MineSweeperGame {
 
         if (board[row][col].isMine())   // did I lose
             status = GameStatus.Lost;
-        else { // every cell exposed?
-
-            status = GameStatus.WON;    // did I win
+        else if (board[row][col].getMineCount() == 0)
+            revealEmptyCellsRecursive();
+            if (everyNonMineCellExposed()) { // every cell exposed?
+                status = GameStatus.WON;    // did I win
 
         }
     }
@@ -110,19 +111,19 @@ public class MineSweeperGame {
      *****************************************************************/
     public int neighborCount(int row, int col) {
         int count = 0;
-        if (!board[row][col].isMine()) {
-            for (int scannedRow = row - 1; scannedRow >= row + 1;
-                 scannedRow--) {
+            for (int scannedRow = row - 1; scannedRow <= row + 1;
+                 scannedRow++) {
                 for (int scannedCol = col - 1; scannedCol <=
                         col + 1; scannedCol++) {
-                    if ((row > 0 || row < board.length) && (col > 0 || col < board[row].length))
+                    if ((scannedRow >= 0 && scannedRow < board.length)
+                            && (scannedCol >= 0 && scannedCol < board[row].length)){
                         if (board[scannedRow][scannedCol].isMine())
                             count++;
+                    }
                 }
             }
+            return count;
         }
-        return count;
-    }
 
     public void countMineNeighborsOfAllCells() {
         for (int r = 0; r < boardSize; r++) {
@@ -131,6 +132,21 @@ public class MineSweeperGame {
                     board[r][c].setMineCount(neighborCount(r, c));
             }
         }
+    }
+
+    private boolean everyNonMineCellExposed() {
+        for (int r = 0; r < boardSize; r++) {
+            for (int c = 0; c < boardSize; c++) {
+                if (!board[r][c].isMine())
+                    if (!board[r][c].isExposed())
+                        return false;
+            }
+        }
+        return true;
+    }
+
+    public void revealEmptyCellsRecursive(){
+
     }
 
 }
