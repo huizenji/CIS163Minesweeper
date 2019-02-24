28c5ff18e413e1cb5325f6770ebcbb89bd0fe9f1
freemanlau <47357251+freemanlau@users.noreply.github.com>
2019-02-23 21:55:27 -0500

    Update MineSweeperPanel.java

--- MineSweeperPanel.java
+++ MineSweeperPanel.java
@@ -3,8 +3,7 @@ package project2;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class MineSweeperPanel extends JPanel {
 
@@ -185,5 +184,8 @@ public class MineSweeperPanel extends JPanel {
         }
 
     }
+
+
+
 
 
