761be6dfdbf4c82c1c50964f26d60241b9d37ff5
huizenji <47357244+huizenji@users.noreply.github.com>
2019-02-25 15:45:34 -0500

    Update MineSweeperGame.java
    Final Updates and Prettymatized for Turn In

--- MineSweeperGame.java
+++ MineSweeperGame.java
@@ -2,34 +2,35 @@ package project2;
 
 import java.util.*;
 
+/**********************************************************************
+ * A class that stores all of the methods, rules, and functions of the
+ * MineSweeper game
+ *
+ * Jillian Huizenga and Lauren Freeman
+ * 25 February 2019
+ *********************************************************************/
 public class MineSweeperGame {
 
-    /**
-     * a double array of cells
-     **/
+    /** a double array of cells **/
     private Cell[][] board;
 
-    /**
-     * an object of GameStatus
-     **/
+    /** an object of GameStatus **/
     private GameStatus status;
 
-    /**
-     * an integer that stores the mine count
-     **/
+    /** an integer that stores the mine count **/
     private int mineCount;
 
-    /**
-     * an integer that stores the board size
-     **/
+    /** an integer that stores the board size **/
     private int boardSize;
 
-    private boolean wantsRecursion;
+    /** a boolean that determines how to reveal cells **/
+    private boolean useRecursion;
 
     /******************************************************************
-     * A constructor of the MineSweeperGame class
-     * @param boardSize stores the desired board size
-     * @param mineCount stores the desired mine count
+     * A constructor of the MineSweeperGame class that initializes
+     * the board
+     * @param boardSize stores the desired board size (int)
+     * @param mineCount stores the desired mine count (int)
      *****************************************************************/
     public MineSweeperGame(int boardSize, int mineCount) {
         status = GameStatus.NotOverYet;
@@ -42,33 +43,58 @@ public class MineSweeperGame {
 
     /******************************************************************
      * A private method that sets every cell's boolean conditions
-     * (exposed, exposedRec, exposedNonRec, mine, flagged) to false
+     * (exposed, exposedRec, marked, mine, flagged) to false
      *****************************************************************/
     private void setEmpty() {
         for (int r = 0; r < boardSize; r++)
             for (int c = 0; c < boardSize; c++)
-                board[r][c] = new Cell(false, false, false, false, false);
+                board[r][c] = new Cell(false, false, false, false,
+                        false);
     }
 
     /******************************************************************
-     * A public method that returns a given cell in the board[][] array
-     * @param row stores an integer representing the given row
-     * @param col stores and integer representing the given column
+     * This method returns a Cell for a given row and col on the board
+     * so the panel can make the appropriate display
+     * @param row stores an integer representing a given row (int)
+     * @param col stores and integer representing a given column (int)
      * @return a cell object
      *****************************************************************/
     public Cell getCell(int row, int col) {
         return board[row][col];
     }
 
-    public boolean wantsRecursion() {
-        return wantsRecursion;
+    /******************************************************************
+     * A private method that returns whether to use recursion or
+     * non-recursion to reveal empty cells
+     * @return a boolean called useRecursion
+     *****************************************************************/
+    private boolean useRecursion() {
+        return useRecursion;
     }
 
-    public void setWantsRecursion(boolean wantsRecursion) {
-        this.wantsRecursion = wantsRecursion;
+    /******************************************************************
+     * A method called within MineSweeperPanel that sets whether to use
+     * recursion or non-recursion to reveal empty cells
+     * @param useRecursion an indicated boolean, true for recursion,
+     *                      false for non-recursion
+     *****************************************************************/
+    public void setUseRecursion(boolean useRecursion) {
+        this.useRecursion = useRecursion;
     }
 
+    /******************************************************************
+     * This method is called from the MineSweeperPanel class and is
+     * invoked when the user has selected a JButton within the 2-Dim
+     * array at location row, col.  This method marks that cell.
+     * (contains inner comments)
+     * @param row the first dimensional location of the Cell object
+     *            (int)
+     * @param col the second dimensional location of the Cell object
+     *            (int)
+     *****************************************************************/
     public void select(int row, int col) {
+
+        // cannot lose on first selection, move mine
         if (MineSweeperPanel.actionCounter == 0) {
             moveMine(row, col);
             countMineNeighborsOfAllCells();
@@ -76,20 +102,32 @@ public class MineSweeperGame {
 
         board[row][col].setExposed(true);
 
-        if (board[row][col].isMine())   // did I lose
+        // did I lose (clicked a mine)
+        if (board[row][col].isMine())
             status = GameStatus.Lost;
+
+        // reveal cells using recursion or non-recursion
         if (board[row][col].getMineCount() == 0) {
-            if (wantsRecursion())
+            if (useRecursion())
                 revealEmptyCellsRecursive(row, col);
             else
                 revealEmptyCellsNonRecursive(row, col);
         }
-        if (everyNonMineCellExposed()) { // every cell exposed?
-            status = GameStatus.WON;    // did I win
+
+        // did I win (every non-mine exposed)
+        if (everyNonMineCellExposed()) {
+            status = GameStatus.WON;
         }
     }
 
-    public void moveMine(int row, int col) {
+    /******************************************************************
+     * A private method that searches for a new cell (starting in the
+     * upper left hand corner) to place a mine if first cell clicked
+     * contains a mine
+     * @param row the selected cell's first dimensional position (int)
+     * @param col the selected cell's second dimensional position (int)
+     *****************************************************************/
+    private void moveMine(int row, int col) {
         if (board[row][col].isMine()) {
             board[row][col].setMine(false);
             for (int r = 0; r < board.length; r++) {
@@ -104,22 +142,36 @@ public class MineSweeperGame {
         }
     }
 
+    /******************************************************************
+     * This method is called from the MineSweeperPanel class and it
+     * determines if a player has won the game after the select method
+     * is called
+     * @return the current status of the game (won, lost, notOverYet)
+     *****************************************************************/
     public GameStatus getGameStatus() {
         return status;
     }
 
+    /******************************************************************
+     * This method is called from the MineSweeperPanel class and it
+     * resets the board to a new game
+     *****************************************************************/
     public void reset() {
         MineSweeperPanel.actionCounter = 0;
         status = GameStatus.NotOverYet;
         setEmpty();
         layMines(mineCount);
-
     }
 
+    /******************************************************************
+     * A private method that randomly assigns mines to cells within the
+     * board according to the indicated mineCount
+     * @param mineCount the desired number of mines (int)
+     *****************************************************************/
     private void layMines(int mineCount) {
-        int i = 0;        // ensure all mines are set in place
-
+        int i = 0;
         Random random = new Random();
+
         while (i < mineCount) {
             int c = random.nextInt(boardSize);
             int r = random.nextInt(boardSize);
@@ -132,13 +184,13 @@ public class MineSweeperGame {
     }
 
     /******************************************************************
-     * This method counts the number of mines directly surrounding the
-     * indicated cell while making sure no outOfBounds exceptions are
-     * thrown
-     * @param row the indicated row of the board array
-     * @param col the indicated column of the board array
+     * This private method counts the number of mines directly
+     * surrounding the indicated cell while making sure no outOfBounds
+     * exceptions are thrown
+     * @param row the indicated row of the board array (int)
+     * @param col the indicated column of the board array (int)
      * @return the count of mines directly surrounding the indicated
-     *          cell
+     *          cell (int)
      *****************************************************************/
     private int neighborCount(int row, int col) {
         int count = 0;
@@ -147,7 +199,8 @@ public class MineSweeperGame {
             for (int scannedCol = col - 1; scannedCol <=
                     col + 1; scannedCol++) {
                 if ((scannedRow >= 0 && scannedRow < board.length)
-                        && (scannedCol >= 0 && scannedCol < board[row].length)) {
+                        && (scannedCol >= 0 && scannedCol <
+                        board[row].length)) {
                     if (board[scannedRow][scannedCol].isMine())
                         count++;
                 }
@@ -156,6 +209,10 @@ public class MineSweeperGame {
         return count;
     }
 
+    /******************************************************************
+     * This private method counts the number of neighboring mines for
+     * all cells and assigns them with the value
+     *****************************************************************/
     private void countMineNeighborsOfAllCells() {
         for (int r = 0; r < boardSize; r++) {
             for (int c = 0; c < boardSize; c++) {
@@ -165,6 +222,12 @@ public class MineSweeperGame {
         }
     }
 
+    /******************************************************************
+     * A private method that determines if a player has won the game.
+     * It determines if all cells not containing mines have been
+     * exposed
+     * @return a boolean the represents if the player has won the game
+     *****************************************************************/
     private boolean everyNonMineCellExposed() {
         for (int r = 0; r < boardSize; r++) {
             for (int c = 0; c < boardSize; c++) {
@@ -176,6 +239,12 @@ public class MineSweeperGame {
         return true;
     }
 
+    /******************************************************************
+     * A private method called by revealEmptyCellsRecursive that
+     * exposes the eight cells directly surrounding the indicated cell
+     * @param row the first dimensional location of the cell (int)
+     * @param col the second dimensional location of the cell (int)
+     *****************************************************************/
     private void exposeEightSurroundingCells(int row, int col) {
         if (board[row][col].getMineCount() == 0) {
             for (int scannedRow = row - 1; scannedRow <= row + 1;
@@ -183,70 +252,108 @@ public class MineSweeperGame {
                 for (int scannedCol = col - 1; scannedCol <=
                         col + 1; scannedCol++) {
                     if ((scannedRow >= 0 && scannedRow < board.length)
-                            && (scannedCol >= 0 && scannedCol < board[row].length)) {
+                            && (scannedCol >= 0 && scannedCol <
+                            board[row].length)) {
                         if (!board[scannedRow][scannedCol].isFlagged())
-                            board[scannedRow][scannedCol].setExposed(true);
+                            board[scannedRow][scannedCol]
+                                    .setExposed(true);
                     }
                 }
             }
         }
     }
 
-//    private boolean anyMarked() {
-//        for (int r = 0; r < boardSize; r++) {
-//            for (int c = 0; c < boardSize; c++) {
-//                if (board[r][c].isMarked())
-//                    return true;
-//            }
-//        }
-//        return false;
-//    }
+    /******************************************************************
+     * If the selected cell is blank (i.e., a cell’s mine count is
+     * zero), this will show all blank cells (zero mine count)
+     * connected to the current cell until you find a non-blank
+     * (Non-Recursive)
+     * (contains inner comments)
+     * @param startingRow the selected cell's first dimensional
+     *                    location (int)
+     * @param startingCol the selected cell's second dimensional
+     *                    location (int)
+     *****************************************************************/
+    private void revealEmptyCellsNonRecursive(int startingRow,
+                                              int startingCol) {
 
-    private void revealEmptyCellsNonRecursive(int startingRow, int startingCol) {
+        // marked means the cell needs attention (mC = 0, not exposed)
         board[startingRow][startingCol].setMarked(true);
         int r = 0;
         int c = 0;
+
+        // scan the board for a mark
         while (r < boardSize) {
-            System.out.println("c at beginning of r loop: " + c);
             while (c < boardSize) {
-                if (board[r][c].isMarked()) { //is it marked?
-                    System.out.println("Mark exists");
-                    for (int scannedRow = r - 1; scannedRow <= r + 1; //then look at everything around it
+
+                // found a mark?
+                if (board[r][c].isMarked()) {
+
+                    // then look at everything around it
+                    for (int scannedRow = r - 1; scannedRow <= r + 1;
                          scannedRow++) {
                         for (int scannedCol = c - 1; scannedCol <=
                                 c + 1; scannedCol++) {
-                            if ((scannedRow >= 0 && scannedRow < board.length)
-                                    && (scannedCol >= 0 && scannedCol < board[r].length)) {
-                                if (board[scannedRow][scannedCol].getMineCount() == 0) { // need to be marked?
-                                    if (!board[scannedRow][scannedCol].isExposed())
-                                        board[scannedRow][scannedCol].setMarked(true); // then mark it, we'll be back for it later
-                                }else
-                                    board[scannedRow][scannedCol].setExposed(true);
+                            if ((scannedRow >= 0 && scannedRow <
+                                    board.length)
+                                    && (scannedCol >= 0 && scannedCol <
+                                    board[r].length)) {
+
+                                // does it need to be marked?
+                                if (board[scannedRow][scannedCol]
+                                        .getMineCount() == 0) {
+                                    if (!board[scannedRow][scannedCol]
+                                            .isExposed()) {
+                                        if (!board[scannedRow]
+                                                [scannedCol]
+                                                .isFlagged())
+
+                                            // then mark it, we'll be
+                                            // back for it later
+                                            board[scannedRow]
+                                                    [scannedCol]
+                                                    .setMarked(true);
+                                    }
+                                } else
+
+                                    // no mark needed? just expose
+                                    if (!board[scannedRow][scannedCol]
+                                            .isFlagged())
+                                        board[scannedRow][scannedCol]
+                                                .setExposed(true);
                             }
                         }
                     }
-                    board[r][c].setExposed(true); // we're done with you, un-mark
+
+                    // un-mark the original cell and expose it
+                    board[r][c].setExposed(true);
                     board[r][c].setMarked(false);
+
+                    // re-scan the board for marks
                     r = 0;
                     c = 0;
+
+                    // if cell is not marked, then keep searching
                 } else {
                     c++;
-                    System.out.println("c = " + c);
                 }
             }
             if (c == boardSize) {
-                System.out.println("Before this, c is " + c);
                 c = 0;
-                System.out.println("C is " + c);
                 r++;
-                System.out.println("r = " + r);
             }
         }
+        // loop will end when all marked cells have been attended to
     }
 
-
-
-
+    /******************************************************************
+     * If the selected cell is blank (i.e., a cell’s mine count is
+     * zero), this method will show all blank cells (zero mine count)
+     * connected to the current cell until you find a non-blank
+     * (Recursive)
+     * @param row the selected cell's first dimensional location (int)
+     * @param col the selected cell's second dimensional location (int)
+     *****************************************************************/
     private void revealEmptyCellsRecursive(int row, int col) {
         if (!board[row][col].isFlagged()) {
             exposeEightSurroundingCells(row, col);
@@ -256,15 +363,20 @@ public class MineSweeperGame {
                 for (int scannedCol = col - 1; scannedCol <=
                         col + 1; scannedCol++) {
                     if ((scannedRow >= 0 && scannedRow < board.length)
-                            && (scannedCol >= 0 && scannedCol < board[row].length)) {
-                        if (board[scannedRow][scannedCol].getMineCount() == 0) {
-                            if (!board[scannedRow][scannedCol].isExposedRec())
-                                revealEmptyCellsRecursive(scannedRow, scannedCol);
+                            && (scannedCol >= 0 && scannedCol < 
+                                board[row].length)) {
+                        if (board[scannedRow][scannedCol]
+                            .getMineCount() == 0) {
+                            if (!board[scannedRow][scannedCol]
+                                .isExposedRec())
+                                revealEmptyCellsRecursive(scannedRow,
+                                                          scannedCol);
                         }
                     }
                 }
             }
         }
     }
+
 
 }
