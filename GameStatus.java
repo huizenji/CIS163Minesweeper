ba1c45013b3f28a3f4708b24549439fd34ae46b4
huizenji <47357244+huizenji@users.noreply.github.com>
2019-02-25 16:28:38 -0500

    Update GameStatus.java
    Prettymatized

--- GameStatus.java
+++ GameStatus.java
@@ -1,6 +1,13 @@
-package W19Project2GIVETOSTUDENTS;
+package project2;
 
-
+/**********************************************************************
+ * A class that stores the three game statuses of MineSweeper 
+ * (Lost, WON, and NotOverYet)
+ * 
+ * Jillian Huizenga and Lauren Freeman
+ * CIS 163-01 Professor Ferguson
+ * 26 February 2019
+ *********************************************************************/
 public enum GameStatus {
-    Lost, Won, NotOverYet
+    Lost, WON, NotOverYet
 }
